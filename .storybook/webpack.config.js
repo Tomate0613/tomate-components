456753fe6923a49218522b0f0d9c12c87cdbefb2
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-05-30 11:26:07 +0200

    Setup storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = ({ config }) => {
+  console.log('Webpack configuration loaded');
+
+  config.module.rules.push({
+    test: /\.(png|jpe?g|gif|webp)$/i,
+    type: 'asset/resource',
+  });
+
+  config.resolve.alias['assets'] = path.resolve(__dirname, '../src/assets');
+
+  return config;
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tomate-components
 A react component library for tomate launcher
+
+Use `npm run storybook` to get started
